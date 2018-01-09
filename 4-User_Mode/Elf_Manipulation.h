1f2c50f17869ce99cea1a64df0b06c12fa954ef1
hark130 <hark130@yahoo.com>
2018-01-08 21:32:57 -0600

    4-5-2: Elf_Manipulation Initial Commit
    I began writing an Elf Manipulation library but then I decided I wanted to abstract-away the act of mapping files into memory.  I just want it to "happen".  This got put on the back-burner while I began working on Map_Memory.

---
+++ 4-User_Mode/Elf_Manipulation.h
@@ -0,0 +1,9 @@
+#ifndef __ELF_MANIPULATION__
+#define __ELF_MANIPULATION__
+
+#include "Map_Memory.h"
+#include <stdbool.h>		// bool, true, false
+
+bool is_elf(mapMem_ptr file);
+
+#endif  // __ELF_MANIPULATION__
