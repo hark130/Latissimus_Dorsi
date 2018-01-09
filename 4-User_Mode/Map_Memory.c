0c81173a5a3e8b33fc4b73735743e942ffd90d3a
Joseph Harkleroad <hark130@yahoo.com>
2018-01-09 11:36:24 -0600

    4-5-2: map_file() defined in Map_Mem.c
    map_file() defined in Map_Memory.c.  Function was written offline and has not yet been tested.

--- 4-User_Mode/Map_Memory.c
+++ 4-User_Mode/Map_Memory.c
@@ -1,6 +1,9 @@
 #include "Map_Memory.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <sys/mman.h>	// mmap
+#include <sys/stat.h>	// fstat
 
 #ifndef MAX_TRIES
 // MACRO to limit repeated allocation attempts
@@ -35,25 +38,112 @@ mapMem_ptr create_mapMem_ptr(void)
 		mapMem_ptr->fileMem_ptr must be free()'d by the calling function
  */
 mapMem_ptr map_file(const char* filename)
-{
-	// 1. Create mapMem_ptr
-	// 2. Get file descriptor for filename
-	// 3. Map the file descriptor into memory
-	// 4. Close the file descriptor (mapping into memory increments the file's reference count)
-	// 5. Populate mapMem_ptr
-	// 6. Return
-	/*
-		#include <sys/mman.h>
+{	
+	// LOCAL VARIABLES
+	mapMem_ptr retVal = NULL;
+	int fileDesc = -1;  // Set to error value by default for the purposes of clean-up
+	struct stat fileStat;
+	
+	// INPUT VALIDATION
+	if (NULL == filename)
+	{
+		fprintf(stderr, "map_file() - filename is NULL\n");
+		return retVal;
+	}
+	else if (0 == strlen(filename))
+	{
+		fprintf(stderr, "map_file() - filename is empty\n");
+		return retVal;
+	}
+	else
+	{	
+		// MAP FILE INTO MEMORY
+		// 1. Create mapMem_ptr
+		retVal = create_mapMem_ptr();
+		if (NULL == retVal)
+		{
+			fprintf(stderr, "map_file() - create_mapMem_ptr() returned NULL\n");
+		}
+		else
+		{
+			// 2. Get file descriptor for filename
+			fileDesc = open(filename, O_RDWR);
+			if (0 > fileDesc)
+			{
+				fprintf(stderr, "map_file() - unable to open '%s'\n", filename);
+			}
+			else
+			{
+				// 3. Verify the file descriptor is a regular file
+				// 3.1. Get the file status
+				if (0 != fstat(fileDesc, &fileStat))
+				{
+					fprintf(stderr, "map_file() - unable to fstat '%s'\n", filename);
+				}
+				else
+				{
+					// 3.2. Verify the file status indicates this is a regular file
+					if (!S_ISREG(fileStat.st_mode))
+					{
+						fprintf(stderr, "map_file() - '%s' is not a regular file\n", filename);
+					}
+					else
+					{
+						if (0 >= fileStat.st_size)
+						{
+							fprintf(stderr, "map_file() - Invalid size for '%s'\n", filename);
+						}
+						else
+						{
+							// 3.3. Populate mapMem struct with file size
+							mapMem_ptr->memSize = fileStat.st_size;
+							// 4. Map the file descriptor into memory
+							/*
+								void * mmap (void *addr,
+											 size_t len,
+											 int prot,
+											 int flags,
+											 int fd,
+											 off_t offset);
+							 */
+							mapMem_ptr->fileMem_ptr = mmap(NULL, \
+														   mapMem_ptr->memSize, \
+														   PROT_READ | PROT_WRITE | PROT_EXEC, \
+														   MAP_SHARED, \
+														   fileDesc, \
+														   0);
+							if (NULL == mapMem_ptr->fileMem_ptr)
+							{
+								fprintf(stderr, "map_file() - mmap failed to map file descriptor %d into memory\n", fileDesc);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	// CLEANUP
+	// 1. Check the struct
+	if (retVal)
+	{
+		// Struct was allocated
+		if (NULL == retVal->fileMem_ptr || retVal->memSize <= 0)
+		{
+			// Something went wrong while populating the struct
+			free_struct(&retVal);
+		}
+	}
+	// 2. Check the file descriptor
+	if (fileDesc > -1)
+	{
+		// Close it
+		close(fileDesc);
+		fileDesc = -1;
+	}
 
-		void * mmap (void *addr,
-					 size_t len,
-					 int prot,
-					 int flags,
-					 int fd,
-					 off_t offset);
-	 */
-	// Protection: PROT_READ | PROT_WRITE | PROT_EXEC
-	// Flags: MAP_SHARED - Writing into the mapping is equivalent to writing to the file
+	// DONE
+	return retVal;
 }
 
 
