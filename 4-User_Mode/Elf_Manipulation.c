9cf39a3a109f90bdd4d96ea7dd0c8ae0fa38b2b5
Joseph Harkleroad <hark130@yahoo.com>
2018-01-09 14:35:58 -0600

    4-5-2: Defined is_elf() in Elf_M.c
    Defined is_elf() in Elf_Manipulation.c.  Written offline and untested.

--- 4-User_Mode/Elf_Manipulation.c
+++ 4-User_Mode/Elf_Manipulation.c
@@ -1,11 +1,36 @@
-/*
-        if (fstat (fd, &sb) == −1) {
-                perror ("fstat");
-                return 1;
-        }
+#include "Map_Memory.h"
+#include <stdbool.h>		// bool, true, false
+#include <stdio.h>          // fprintf
+#include <string.h>         // strstr
 
-        if (!S_ISREG (sb.st_mode)) {
-                fprintf (stderr, "%s is not a file\n", argv[1]);
-                return 1;
+bool is_elf(mapMem_ptr file)
+{
+    // LOCAL VARIABLES
+    bool retVal = false;
+    char* file_ptr = NULL;
+    char* return_ptr = NULL;
+    char magicNum[] = { 0x7F, 'E', 'L', 'F', 0x0 };
+    
+    // INPUT VALIDATION
+    if (NULL != file)
+    {
+        if (NULL != file->fileMem_ptr)
+        {
+            // FIND ELF MAGIC NUMBER
+            return_ptr = strstr(file->fileMem_ptr, magicNum);
+            
+            if (file->fileMem_ptr == return_ptr)
+            {
+                retVal = true;
+            }
+            else if (NULL != return_ptr)
+            {
+                retVal = true;
+                fprintf(stdout, "Found the ELF magic number at 0x%p but the mapped file begins at 0x%p", return_ptr, file->fileMem_ptr);  // DEBUGGING
+            }
         }
- */
+    }
+    
+    // DONE
+    return retVal;
+}
