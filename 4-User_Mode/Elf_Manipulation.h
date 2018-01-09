4fb13ceb0c311485d2e79efe6f57987def382d12
hark130 <hark130@yahoo.com>
2018-01-09 17:36:16 -0600

    4-5-2: Updated Elf_Manipulation.h
    Added comment blocks for the function prototypes.
    Also, added new function prototype of find_code_cave().

--- 4-User_Mode/Elf_Manipulation.h
+++ 4-User_Mode/Elf_Manipulation.h
@@ -4,6 +4,22 @@
 #include "Map_Memory.h"
 #include <stdbool.h>		// bool, true, false
 
+
+/*
+	Purpose - Check an mmap()'d file for the ELF Magic Number
+	Input - file - struct* mappedMemory
+	Output - true if in ELF, otherwise false
+ */
 bool is_elf(mapMem_ptr file);
+
+
+/*
+	Purpose - Search an ELF binary for the largest section of 'nothing'
+	Input - elfBinary - struct* mappedMemory
+	Output - struct* mappedMemory which holds the address and size of the 
+		largest section of 'nothing'
+ */
+mapMem_ptr find_code_cave(mapMem_ptr elfBinary);
+
 
 #endif  // __ELF_MANIPULATION__
