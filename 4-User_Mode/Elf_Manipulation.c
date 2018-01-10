dc3a5d9256792c421dfbd984688125e26c55feef
hark130 <hark130@yahoo.com>
2018-01-09 20:14:07 -0600

    4-5-2: E_M.h find_code_cave() Complete
    The Elf_Manipulation.c function find_code_cave() is tested, passing, and complete.

--- 4-User_Mode/Elf_Manipulation.c
+++ 4-User_Mode/Elf_Manipulation.c
@@ -34,3 +34,92 @@ bool is_elf(mapMem_ptr file)
     // DONE
     return retVal;
 }
+
+
+/*
+    Purpose - Search an ELF binary for the largest section of 'nothing'
+    Input - elfBinary - struct* mappedMemory
+    Output - struct* mappedMemory which holds the address and size of the 
+        largest section of 'nothing'
+ */
+mapMem_ptr find_code_cave(mapMem_ptr elfBinary)
+{
+    // LOCAL VARIABLES
+    mapMem_ptr retVal = NULL;
+    char* tempAddr_ptr = NULL;  // Pointer to the beginning of the current section
+    char* currChar_ptr = NULL;  // Pointer used to validate each char
+    int byteCounter = 0;        // Counts the size of the 'nothing' segment
+
+    // INPUT VALIDATION
+    if (true == validate_struct(elfBinary))
+    {
+        // puts("MADE IT PAST validate_struct()");  // DEBUGGING
+        // 1. Create the return struct
+        retVal = create_mapMem_ptr();
+        retVal->fileMem_ptr = elfBinary->fileMem_ptr;
+        retVal->memSize = 0;
+        retVal->memType = MM_TYPE_CAVE;
+        currChar_ptr = elfBinary->fileMem_ptr;
+        tempAddr_ptr = elfBinary->fileMem_ptr;
+
+        // puts("MADE IT HERE!");  // DEBUGGING
+        // 2. Start counting
+        while (currChar_ptr < (elfBinary->fileMem_ptr + elfBinary->memSize))
+        {
+            // puts("ITERATING!");  // DEBUGGING
+            // putchar(*currChar_ptr);  // DEBUGGING
+            // Found empty char
+            if (0x0 == *currChar_ptr)
+            {
+                // puts("MADE IT HERE!");  // DEBUGGING
+                byteCounter++;
+            }
+            // End of empty chars
+            else
+            {
+                // Larger than value stored in retVal
+                if (byteCounter > retVal->memSize)
+                {
+                    // puts("FOUND ONE!");  // DEBUGGING
+                    // Set struct to beginning of this section
+                    retVal->fileMem_ptr = tempAddr_ptr;
+                    // Set the size
+                    retVal->memSize = byteCounter;
+                    // fprintf(stdout, "Current winner pointer:\t%p\n", retVal->fileMem_ptr);  // DEBUGGING
+                    // fprintf(stdout, "Current winner length:\t%zu\n", retVal->memSize);  // DEBUGGING
+                }
+
+                // Reset...
+                // ...byteCounter
+                byteCounter = 0;
+                // ...tempAddr_ptr
+                tempAddr_ptr = currChar_ptr + 1;
+            }
+
+            // Increment to next byte
+            currChar_ptr++;
+        }
+        // Trailing "code cave"
+        // Larger than value stored in retVal
+        if (byteCounter > retVal->memSize)
+        {
+            // puts("FOUND ONE!");  // DEBUGGING
+            // Set struct to beginning of this section
+            retVal->fileMem_ptr = tempAddr_ptr;
+            // Set the size
+            retVal->memSize = byteCounter;
+            // fprintf(stdout, "Current winner pointer:\t%p\n", retVal->fileMem_ptr);  // DEBUGGING
+            // fprintf(stdout, "Current winner length:\t%zu\n", retVal->memSize);  // DEBUGGING
+        }
+    }
+
+    // Never found one
+    if (0 == retVal->memSize && retVal->fileMem_ptr == elfBinary->fileMem_ptr)
+    {
+        retVal->fileMem_ptr = NULL;
+    }
+
+    // DONE
+    return retVal;
+}
+
