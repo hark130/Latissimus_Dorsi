770a263e505e25ff9646cd409eb4e1a9f9be22e5
Joseph Harkleroad <hark130@yahoo.com>
2018-01-10 11:37:00 -0600

    4-5-2: Updated E_M.h w/structs & prototype
    Updated Elf_Manipulation.h with two new structs:
        Mapped_Memory_Elf32
        Mapped_Memory_Elf64
    These two structs will contain pointers to ELF-related structs for a given mappedMemory file
    
    Also, updated Elf_Manipulation.h with a new prototype: determine_elf_class()
        This new function will determine, programatically, what "class" (see: None, 32-bit, or 64-bit) an ELF file is formatted as.  It is the first step on the path to programatically manipulating ELF files.

--- 4-User_Mode/Elf_Manipulation.h
+++ 4-User_Mode/Elf_Manipulation.h
@@ -1,8 +1,27 @@
 #ifndef __ELF_MANIPULATION__
 #define __ELF_MANIPULATION__
 
+#include <elf.h>
 #include "Map_Memory.h"
 #include <stdbool.h>		// bool, true, false
+
+
+typedef struct Mapped_Memory_Elf32
+{
+	mapMem_ptr binary_ptr;
+	Elf32_Ehdr* binaryEhdr_ptr;
+	Elf32_Phdr* binaryPhdr_ptr;
+	Elf32_Shdr* binaryShdr_ptr;
+} mapElf32, *mapElf32_ptr;
+
+
+typedef struct Mapped_Memory_Elf64
+{
+	mapMem_ptr binary_ptr;
+	Elf64_Ehdr* binaryEhdr_ptr;
+	Elf64_Phdr* binaryPhdr_ptr;
+	Elf64_Shdr* binaryShdr_ptr;
+} mapElf64, *mapElf64_ptr;
 
 
 /*
@@ -11,6 +30,17 @@
 	Output - true if in ELF, otherwise false
  */
 bool is_elf(mapMem_ptr file);
+
+
+/*
+	Purpose - Determine if a mappedMemory file is 32-bit ELF or 64-bit ELF
+	Input - mappedMemory struct pointer
+	Output
+		ELFCLASSNONE if mappedMemory is not an ELF file
+		ELFCLASS32 if mappedMemory is a 32-bit ELF file
+		ELFCLASS64 if mappedMemory is a 64-bit ELF file
+ */
+int determine_elf_class(mapMem_ptr elfFile);
 
 
 /*
