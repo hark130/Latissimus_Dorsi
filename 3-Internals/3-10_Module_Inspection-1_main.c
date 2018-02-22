e64129537c904cad2b28d0b861986258cee7124f
Joseph Harkleroad <hark130@yahoo.com>
2018-02-22 14:46:17 -0600

    3-10-1: main.c Initial Commit
    Barely any code inside the initial commit of main.c.  Mostly comments and a DEBUG statement in case anyone compiles and executes it.

---
+++ 3-Internals/3-10_Module_Inspection-1_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Initial commit
+
+/*
+	1. Programatically determine which applications are running
+	2. Prompt the user to choose an application
+	3. List the modules loaded in that application
+	4. Inspect those modules
+ */
+ 
+ int main(void)
+ {
+ 	puts("main() isn't doing anything yet!");
+ 	return;
+ }
+ 
