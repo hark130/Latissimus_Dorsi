e0011f55bc3fcd49d3d3db23f3fa262549c3bdf6
Joseph Harkleroad <hark130@yahoo.com>
2018-02-22 14:55:55 -0600

    3-10-1: Adding notes to main.c
    Adding notes on how to list processes.

--- 3-Internals/3-10_Module_Inspection-1_main.c
+++ 3-Internals/3-10_Module_Inspection-1_main.c
@@ -3,6 +3,13 @@
 
 /*
 	1. Programatically determine which applications are running
+		1. Terminal
+			1. top
+			2. htop
+			3. ps
+			4. pstree
+			5. pgrep
+			6. 
 	2. Prompt the user to choose an application
 	3. List the modules loaded in that application
 	4. Inspect those modules
