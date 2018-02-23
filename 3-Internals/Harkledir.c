59dde9f59aae1a90bc35ff72137934809a5e82d8
hark130 <hark130@yahoo.com>
2018-02-22 18:37:24 -0600

    3-10-1: Began Hd.c free_dirDetails_ptr()
    Began defining Harkledir.c free_dirDetails_ptr().  Finished dirName but that's it.  Incremental commit.  Dinner time.

--- 3-Internals/Harkledir.c
+++ 3-Internals/Harkledir.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>		// calloc
 #include "Harkledir.h"
 #include <stdbool.h>	// bool, true, false
+#include <string.h>		// memset
 
 
 #ifndef HDIR_MAX_TRIES
@@ -58,4 +59,62 @@ dirDetails_ptr open_dir(char* dirName);
 		Will memset(0x0), free, and NULL any pointers
 		Will zeroize all other members
  */
-bool free_dirDetails_ptr(dirDetails_ptr* oldStruct_ptr);
+bool free_dirDetails_ptr(dirDetails_ptr* oldStruct_ptr)
+{
+	// LOCAL VARIABLES
+	bool retVal = true;
+	dirDetails_ptr oldStruct = NULL;
+	char* temp_ptr = NULL;
+
+	// INPUT VALIDATION
+	if (oldStruct_ptr)
+	{
+		if (*oldStruct_ptr)
+		{
+			oldStruct = *oldStruct_ptr;
+
+			// FREE
+			// 1. char* dirName;
+			if (oldStruct->dirName)
+			{
+				if (*(oldStruct->dirname))
+				{
+					// 1.1. memset dirName
+					temp_ptr = memset(oldStruct->dirName, 0x0, strlen(oldStruct->dirName));
+
+					if (temp_ptr != oldStruct->dirName)
+					{
+						fprintf(stderr, "<<<ERROR>>> - free_dirDetails_ptr() - memset failed to zeroize the struct's dirName\n");
+					}
+
+					// 1.2. free dirName
+					free(oldStruct->dirName);
+
+					// 1.3. Zero dirName
+					oldStruct->dirName = NULL;
+				}
+			}
+
+			// 2. int numFiles;
+			oldStruct->numFiles = 0;
+
+			// char** fileName_arr;
+			// int numDirs;
+			// char** dirName_arr;
+		}
+	}
+
+	// DONE
+	return retVal;
+}
+
+/*
+typedef struct directoryDetails
+{
+	char* dirName;
+	int numFiles;
+	char** fileName_arr;
+	int numDirs;
+	char** dirName_arr;
+} dirDetails, *dirDetails_ptr;
+ */
