1f455646695b6db9fb0335a7e1a0369cdf019709
hark130 <hark130@yahoo.com>
2018-02-24 18:55:31 -0600

    3-10-2: main.c Initial Commit
    Isn't doing anyting yet other than calling puts().  It's waiting on Harkleproc's new pidDetails struct functions to be defined first.

---
+++ 3-Internals/3-10_Proc_Walk-2_main.c
@@ -0,0 +1,13 @@
+#include "Harkledir.h"
+#include "Harkleproc.h"
+#include <stdio.h>
+
+ 
+ int main(void)
+ {
+ 	// LOCAL VARIABLES
+ 	puts("main() isn't doing anything yet!");
+
+	return 0;
+ }
+ 
