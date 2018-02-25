632273979348a04fd9b7c15bdac3e0f5ecce84fe
hark130 <hark130@yahoo.com>
2018-02-24 18:52:55 -0600

    3-10-2: Defined Hp.c c_P_s()
    Defined Harkleproc.c's create_PID_struct().  Definition also contains placeholder-prototypes for the rest of the new functions awaiting definition.  Will not (should not) yet compile.  Not done with the definitions.

--- 3-Internals/Harkleproc.c
+++ 3-Internals/Harkleproc.c
@@ -23,6 +23,80 @@ char* copy_a_name(const char* fileName);
 
 
 /*
+    Purpose - Allocated a harklePIDDetails on the heap
+    Input - None
+    Output - A heap-allocated, zeroized, harklePIDDetails pointer
+    Notes:
+        It is the caller's responsibility to call 
+            free_PID_struct(&pidDetails_ptr) when this struct pointer
+            is not longer needed.
+ */
+pidDetails_ptr create_PID_struct(void)
+{
+    // LOCAL VARIABLES
+    pidDetails_ptr retVal = NULL;
+    int numTries = 0;
+
+    while (!retVal && numTries < HPROC_MAX_TRIES)
+    {
+        retVal = (pidDetails_ptr)calloc(1, sizeof(pidDetails));
+        numTries++;
+    }
+
+    if (!retVal)
+    {
+        fprintf(stderr, "<<<ERROR>>> - Harkleproc - create_PID_struct() - Failed to allocate a pidDetails struct pointer!\n");
+    }
+
+    // DONE
+    return retVal;
+}
+
+
+/*
+    Purpose - Populate a harklePIDDetails struct with PID information
+    Input - None
+    Output
+        A pointer to a heap-allocated harklePIDDetails struct complete
+            with information about the PID found at pidPath on success.
+            NULL on failure.
+    Notes:
+        Returns NULL if pidPath does not begin with /proc
+        Returns NULL if the directory following /proc is not a PID (non-number)
+        Returns a pointer if pidPath is missing but stillExists is False
+ */
+pidDetails_ptr populate_PID_struct(const char* pidPath);
+
+
+/*
+    Purpose - To memset, free, and NULL a harklePIDDetails pointer
+    Input
+        pidDetails_ptr - A pointer to a pidDetails_ptr
+    Output - True on success, False on failure
+    Notes:
+        Will memset each non-empty, non-NULL char* in the struct
+        Will free each non-NULL char* in the struct
+        Will NULL each char* in the struct
+        Will set pidDetails_ptr to NULL when done
+ */
+bool free_PID_struct(pidDetails_ptr* pidDetails_ptr);
+
+
+/*
+    Purpose - To memset, free, and NULL all harklePIDDetails pointers
+        in an array, to include the array itself
+    Input
+        pidDetails_arr - Pointer to a NULL-terminated array of harklePIDDetails pointers
+    Output - True on success, False on failure
+    Notes:
+        Calls free_PID_struct() to free each pidDetails_ptr in the array
+        Will free the array itself
+        Will NULL the array pointer
+ */
+bool free_PID_struct_arr(pidDetails_ptr** pidDetails_arr);
+
+
+/*
 	Purpose - Provide a 'list' of running PIDs based on /proc
 	Input - None
 	Ouput - A NULL-terminated, heap-allocated array of heap-allocated C-strings, one-per-directory
