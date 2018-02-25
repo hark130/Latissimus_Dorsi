50eae8afd70f7b941b7ee7b2c9b8820bd4d16c88
hark130 <hark130@yahoo.com>
2018-02-24 23:31:13 -0600

    3-10-2: Defined main() to do something
    Defined main() to do something with the new functionality in Harkleproc.  Not yet tested, much less compiled.

--- 3-Internals/3-10_Proc_Walk-2_main.c
+++ 3-Internals/3-10_Proc_Walk-2_main.c
@@ -6,8 +6,56 @@
  int main(void)
  {
  	// LOCAL VARIABLES
- 	puts("main() isn't doing anything yet!");
+ 	pidDetails_ptr* procPIDStructs = parse_proc_PID_structs();
+ 	pidDetails_ptr* temp_arr = procPIDStructs;
+ 	pidDetails_ptr tempStruct_ptr = NULL;  // Single struct from the array
+ 	int pidNum = 0;  // Tracks PID count
 
+ 	// INPUT VALIDATION
+ 	if (procPIDStructs)
+ 	{
+ 		while (temp_arr)
+ 		{
+ 			// Get a struct pointer
+ 			tempStruct_ptr = *temp_arr;
+
+ 			if (tempStruct_ptr)
+ 			{
+	 			// PRINT PROC PID DETAILS
+	 			if (tempStruct_ptr->stillExists == true)
+	 			{
+	 				fprintf(stdout, "PID #%d:\t%s - %s\n", ++pidNum, tempStruct_ptr->pidNum, tempStruct_ptr->pidCmdline);
+	 			}
+	 			else
+	 			{
+	 				fprintf(stdout, "PID #%d:\t%s - %s\n", ++pidNum, tempStruct_ptr->pidNum, "No longer exists");
+	 			}
+	 		}
+	 		else
+	 		{
+	 			fprintf(stdout, "PID #%d:\t%s - %s\n", ++pidNum, "NULL", "Empty pidDetails struct");
+	 		}
+
+ 			// Next pidDetails_ptr
+ 			temp_arr++;
+ 		}
+ 	}
+ 	else
+ 	{
+ 		puts("<<<ERROR>>> - main() - parse_proc_PID_structs has failed!");
+ 	}
+ 	
+ 	// CLEAN UP
+ 	if (procPIDStructs)
+ 	{
+ 		if (false == free_PID_struct_arr(&procPIDStructs))
+ 		{
+ 			puts("<<<ERROR>>> - main() - free_PID_struct_arr has failed!");
+ 		}
+ 	}
+ 	temp_arr = NULL;
+
+ 	// DONE
 	return 0;
  }
  
