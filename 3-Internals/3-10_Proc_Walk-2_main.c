c24939fc46a29beccc17e6d86ca03a677a596083
hark130 <hark130@yahoo.com>
2018-02-25 21:53:06 -0600

    3-10-2: Main() now takes user input
    Main() now:
    	- Prompts the user for a PID
    	- Stores the PID
    	- Translates that PID into an absolute path to the <PID> directory
    
    This sounds like a small commit but a lot of Memoroad, Fileroad, and Harkleproc work got done to organize, build, and fix the code necessary to accomplish this relatively easy(?) task.

--- 3-Internals/3-10_Proc_Walk-2_main.c
+++ 3-Internals/3-10_Proc_Walk-2_main.c
@@ -1,20 +1,26 @@
+#include "Fileroad.h"
 #include "Harkledir.h"
 #include "Harkleproc.h"
+#include "Memoroad.h"
 #include <stdio.h>
+#include <stdbool.h>	// bool, true, false
 
  
  int main(void)
  {
  	// LOCAL VARIABLES
+ 	bool success = true;  // If anything fails, set this to false
  	pidDetails_ptr* procPIDStructs = parse_proc_PID_structs();
  	pidDetails_ptr* temp_arr = procPIDStructs;
  	pidDetails_ptr tempStruct_ptr = NULL;  // Single struct from the array
  	int pidNum = 0;  // Tracks PID count
+ 	char* userPID = NULL;  // Will hold char* holding user's choice of PID
+ 	char* userProcPID = NULL;  // Will hold char* holding /proc/<PID>/ built from user's choice
 
  	// INPUT VALIDATION
  	if (procPIDStructs)
  	{
- 		while (*temp_arr)
+ 		while (*temp_arr && success == true)
  		{
  			// Get a struct pointer
  			tempStruct_ptr = *temp_arr;
@@ -43,17 +49,60 @@
  	else
  	{
  		puts("<<<ERROR>>> - main() - parse_proc_PID_structs has failed!");
+ 		success = false;
+ 	}
+
+ 	// PROMPT USER FOR PID
+ 	fprintf(stdout, "Which PID would you like to investigate?\n");
+ 	userPID = buff_a_num();
+ 	if (userPID)
+ 	{
+ 		fprintf(stdout, "The user chose:\t%s\n", userPID);  // DEBUGGING
+
+ 		userProcPID = make_PID_into_proc(userPID);
+
+ 		if (userProcPID)
+ 		{
+ 			fprintf(stdout, "Absolute path:\t%s\n", userProcPID);  // DEBUGGING
+ 		}
+ 		else
+ 		{
+ 			fprintf(stderr, "<<<ERROR>>> - main() - make_PID_into_proc has failed!");
+ 		}
+ 	}
+ 	else
+ 	{
+ 		fprintf(stderr, "<<<ERROR>>> - main() - buff_a_num has failed!");
  	}
  	
  	// CLEAN UP
+ 	// 1. procPIDStructs
  	if (procPIDStructs)
  	{
  		if (false == free_PID_struct_arr(&procPIDStructs))
  		{
- 			puts("<<<ERROR>>> - main() - free_PID_struct_arr has failed!");
+ 			fprintf(stderr, "<<<ERROR>>> - main() - free_PID_struct_arr has failed!");
  		}
  	}
  	temp_arr = NULL;
+
+ 	// 2. userPID
+ 	if (userPID)
+ 	{
+ 		if (false == release_a_string(&userPID))
+ 		{
+ 			fprintf(stderr, "<<<ERROR>>> - main() - release_a_string has failed!");
+ 		}
+ 	}
+
+ 	// 3. userProcPID
+ 	if (userProcPID)
+ 	{
+ 		if (false == release_a_string(&userProcPID))
+ 		{
+ 			fprintf(stderr, "<<<ERROR>>> - main() - release_a_string has failed!");
+ 		}
+ 	}
 
  	// DONE
 	return 0;
