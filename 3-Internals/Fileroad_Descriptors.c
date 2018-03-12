6d1ac8f67e55a0a52bd6a6cd431ef4b2d1302a7a
Joseph Harkleroad <hark130@yahoo.com>
2018-03-12 16:31:19 -0500

    3-3-2: Fr_d.c initial rBindDat struct definitions
    Fileroad_Descriptords.c Initial Definition:
        - Added new function prototypes as place holders for definitions
        - Removed function comment blocks from fdDetails functions (they should only be in the header)
        - Added the same comment block organizers from the header to help organize the functions here

--- 3-Internals/Fileroad_Descriptors.c
+++ 3-Internals/Fileroad_Descriptors.c
@@ -11,24 +11,65 @@
 #define FD_MAX_TRIES 3
 #endif  // FD_MAX_TRIES
 
-/*
-typedef struct fileDescriptorDetails
+typedef struct redirectBinOutput
 {
-    char* filename_ptr;     // Path to file
-    int fileDesc;           // File descriptor
-    uintmax_t fileSize;     // Actual size of file
-    uintmax_t diskSize;     // Size of file on disk
-} fdDetails, *fdDetails_ptr;
- */
+	char* binName;			// Just the binary name
+	//char* binPath;  // Implement later?
+	char* outputFile;		// File capturing binary's stdout
+	char* errorsFile;		// File capturing binary's stderr
+	int readPipe;			// Implement later... Binary (child) reads stdin from here
+	int writePipe;			// Implement later... redirect_bin_output.exe (parent) writes binary's input here
+} rBinDat, *rBinDat_ptr;
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////// rBinDat FUNCTIONS START //////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 
 /*
 	Purpose - Dynamically allocate a struct pointer
 	Input - None
-	Output - Pointer to a fileDescriptorDetails struct on the heap
+	Output - Pointer to a redirectBinOutput struct on the heap
 	Notes:
-		fdDetails_ptr must be free()'d by the calling function
+		rBinDat_ptr must be free()'d by the calling function
  */
+rBinDat_ptr create_rBinDat_ptr(void);
+
+
+/*
+	Purpose - Populate a redirectBinOutput struct
+	Input
+		binaryName - Relative or absolute path string to the binary being redirected
+	Output - Pointer to a redirectBinOutput struct on the heap whose binName is
+		populated
+	Notes:
+		Calls create_rBinDat_ptr() to allocate the struct
+		Caller is responsible for free()ing all char pointer members and the 
+			struct itself
+ */
+rBinDat_ptr build_rBinDat_ptr(const char* binaryName);
+
+
+/*
+	Purpose - Free a redirectBinOutput struct on the heap
+	Input - Pointer to a redirectBinOutput pointer to free
+	Output - None
+	Notes:
+		Will memset(0x0), free, and NULL all char pointer members
+		Will zeroize all other members
+ */
+bool free_rBinDat_ptr(&rBinDat_ptr);
+
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////// rBinDat FUNCTIONS STOP ///////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+////////////////////////// fdDetails FUNCTIONS START /////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
 fdDetails_ptr create_fdDetails_ptr(void)
 {
     // LOCAL VARIABLES
@@ -47,18 +88,6 @@ fdDetails_ptr create_fdDetails_ptr(void)
 }
 
 
-/*
-	Purpose - Open a file and populate a fileDescriptorDetails struct
-	Input
-		fname - Filename to open
-		flags - Access modes: O_RDONLY, O_WRONLY, or O_RDWR
-		mode - A bitwise OR of file creation flags and file status flags
-	Output - Pointer to a fileDescriptorDetails struct
-	Notes:
-		fdDetails_ptr must be free()'d by the calling function
-		fdDetails_ptr->filename_ptr must be free()'d by the calling function
-        This function calls create_fdDetails_ptr() to allocate a struct
- */
 fdDetails_ptr open_fd(const char* fname, int flags, mode_t mode)
 {
     // LOCAL VARIABLES
@@ -181,16 +210,6 @@ fdDetails_ptr open_fd(const char* fname, int flags, mode_t mode)
 }
 
 
-/*
-	Purpose - Free a fileDescriptorDetails struct on the heap
-	Input - Pointer to a fileDescriptorDetails pointer to free
-	Output - None
-	Notes:
-		Will memset(0x0), free, and NULL filename_ptr
-		Will zeroize all other members
-		Closing the file descriptor is the responsibility of the
-            calling function
- */
 void free_fdDetails(fdDetails_ptr* oldStruct_ptr)
 {
     // LOCAL VARIABLES
@@ -256,14 +275,6 @@ void free_fdDetails(fdDetails_ptr* oldStruct_ptr)
 }
 
 
-/*
-	Purpose - Validate the contents of a fileDescriptorDetails struct pointer
-	Input - Pointer to a fileDescriptorDetails struct
-	Output - true if valid, otherwise false
-	Notes:
-		Can not validate memType against the type of memory allocated
-		Merely checks that memType is valid
- */
 bool validate_fdDetails(fdDetails_ptr checkThis_ptr)
 {
     // LOCAL VARIABLES
@@ -274,11 +285,6 @@ bool validate_fdDetails(fdDetails_ptr checkThis_ptr)
 }
 
 
-/*
-    Purpose - Update the details of a fileDescriptorDetails struct pointer
-    Input - Pointer to a fileDescriptorDetails struct
-    Output - 0 on success, error code on failure
- */
 int update_fdDetails(fdDetails_ptr updateThis_ptr)
 {
     // LOCAL VARIABLES
@@ -289,16 +295,6 @@ int update_fdDetails(fdDetails_ptr updateThis_ptr)
 }
 
 
-/*
-	Purpose - Set an I/O Operating Mode flag(s) for a file descriptor
-	Input
-		updateThis_ptr - fileDescriptorDetails struct pointer
-		setThisFlag - Operating mode flag(s) to set for updateThis_ptr's fd
-	Output - true on success, false on failure
-	Notes:
-		This function will preserve the unchanged flags
-		This function calls write_oper_mode_flags()
- */
 bool set_oper_mode_flag(fdDetails_ptr updateThis_ptr, int setThisFlag)
 {
 	// LOCAL VARIABLES
@@ -344,16 +340,6 @@ bool set_oper_mode_flag(fdDetails_ptr updateThis_ptr, int setThisFlag)
 }
 
 
-/*
-	Purpose - Clear an I/O Operating Mode flag(s) for a file descriptor
-	Input
-		updateThis_ptr - fileDescriptorDetails struct pointer
-		clrThisFlag - Operating mode flag(s) to set for updateThis_ptr's fd
-	Output - true on success, false on failure
-	Notes:
-		This function will preserve the unchanged flags
-		This function calls write_oper_mode_flags()
- */
 bool clear_oper_mode_flag(fdDetails_ptr updateThis_ptr, int clrThisFlag)
 {
 	// LOCAL VARIABLES
@@ -399,17 +385,6 @@ bool clear_oper_mode_flag(fdDetails_ptr updateThis_ptr, int clrThisFlag)
 }
 
 
-/*
-	Purpose - Overwrite all I/O Operating Mode flags for a file descriptor
-	Input
-		updateThis_ptr - fileDescriptorDetails struct pointer
-		setTheseFlags - Operating mode flags to set for updateThis_ptr's fd
-	Output - true on success, false on failure
-	Notes
-		This function does not preserve original flags, it overwrites flags
-		fcntl() ignores certain flags in arg, only the following can be changed:
-			O_APPEND, O_ASYNC, O_DIRECT, O_NOATIME, and O_NONBLOCK 
- */
 bool write_oper_mode_flags(fdDetails_ptr updateThis_ptr, int setTheseFlags)
 {
 	// LOCAL VARIABLES
@@ -433,13 +408,15 @@ bool write_oper_mode_flags(fdDetails_ptr updateThis_ptr, int setTheseFlags)
 }
 
 
-/*
-	Purpose - Determine the file size of a file descriptor
-	Input - An open file descriptor
-	Output - Size of the file descriptor
-	Notes:
-		This function will preserve the file offset
- */
+//////////////////////////////////////////////////////////////////////////////
+////////////////////////// fdDetails FUNCTIONS STOP //////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////// GENERAL FUNCTIONS START //////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
 off_t get_file_len(int fileDesc)
 {
 	// LOCAL VARIABLES
@@ -471,6 +448,10 @@ off_t get_file_len(int fileDesc)
 	return retVal;
 }
 
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////////// GENERAL FUNCTIONS STOP ///////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 /*
 // For later
