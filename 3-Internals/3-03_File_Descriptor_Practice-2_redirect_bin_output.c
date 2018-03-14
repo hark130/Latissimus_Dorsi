76a265ec3bbed091eacf461a24cdd1c5984fe93e
Joseph Harkleroad <hark130@yahoo.com>
2018-03-14 11:10:23 -0500

    3-3-2: Added conditional DEBUGGING to r_b_o.c
    Added some HARKLE_DEBUG conditional compilation statements surrounding some old "user feedback" I had previously silenced.  redirect_bin_output.exe is entirely silent during error-free execution.  I liked that but I also wanted the option for additional output.  I toyed around with the idea of trying to add "arguments" to the execution of redirect_bin_output.exe but that would have proved problematic given argv + 1 is copied and used as parameters for the eventual execvp() function call.  The solution came to me as I was cleaning up this code's style.  I already have a "DEBUG" macro in Harklerror.h, why not use it to conditionally compile that final human-readable output from a successful run?  (Plus, I already made mention to the self-same macro in the program's wiki page (man-page-style listing).  Let's see how this works out.

--- 3-Internals/3-03_File_Descriptor_Practice-2_redirect_bin_output.c
+++ 3-Internals/3-03_File_Descriptor_Practice-2_redirect_bin_output.c
@@ -173,25 +173,27 @@ int main(int argc, char* argv[])
 		}		
 	}
 	
+#ifdef HARKLE_DEBUG
 	// PRINT THE RESULTS
-	// if (success == true)
-	// {
-	// 	temp_arr = silentBin->fullCmd;		
-	// 	if (temp_arr)
-	// 	{
-	// 		fprintf(stdout, "%s was run with the following syntax:\n\t", silentBin->binName);
+	if (success == true)
+	{
+		temp_arr = silentBin->fullCmd;		
+		if (temp_arr)
+		{
+			fprintf(stdout, "%s was run with the following syntax:\n\t", silentBin->binName);
 			
-	// 		while (*temp_arr)
-	// 		{
-	// 			fprintf(stdout, "%s ", *temp_arr);
-	// 			temp_arr++;
-	// 		}
-	// 		fprintf(stdout, "\n");
-	// 	}
-	// 	fprintf(stdout, "%s's output was captured in the following files:\n\n", silentBin->binName);  // DEBUGGING
-	// 	fprintf(stdout, "**************\n*** STDOUT ***\n**************\n(%s)\n%s\n", silentBin->outputFile, binOutResults);  // DEBUGGING
-	// 	fprintf(stdout, "**************\n*** STDERR ***\n**************\n(%s)\n%s\n\n\n", silentBin->errorsFile, binErrResults);  // DEBUGGING
-	// }
+			while (*temp_arr)
+			{
+				fprintf(stdout, "%s ", *temp_arr);
+				temp_arr++;
+			}
+			fprintf(stdout, "\n");
+		}
+		fprintf(stdout, "%s's output was captured in the following files:\n\n", silentBin->binName);  // DEBUGGING
+		fprintf(stdout, "**************\n*** STDOUT ***\n**************\n(%s)\n%s\n", silentBin->outputFile, binOutResults);  // DEBUGGING
+		fprintf(stdout, "**************\n*** STDERR ***\n**************\n(%s)\n%s\n\n\n", silentBin->errorsFile, binErrResults);  // DEBUGGING
+	}
+#endif  // HARKLE_DEBUG
 	
 	// CLEAN UP
 	// 1. rBinDat_ptr silentBin = NULL;  // Return value from build_rBinDat_ptr()
@@ -325,7 +327,6 @@ char* build_filename(char* timestamp, char* properName, bool output)
 						}
 					}
 				}
-
 			}
 		}
 	}
