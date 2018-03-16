e2145e591c159ae5cab15b973adbfd2e9086a09d
hark130 <hark130@yahoo.com>
2018-03-15 23:44:56 -0500

    3-4-1: Update to Waiting.exe
    Added a self-destruct counter so that Waiting.exe wouldn't actually "continue forever" if I was trying to valgrind it.
    
    SPOILER ALERT:  Valgrind doesn't like kill signals.

--- 3-Internals/3-04_Signal_Handling-1_Waiting.c
+++ 3-Internals/3-04_Signal_Handling-1_Waiting.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+	int i = 0;
 	printf("Waiting...");
 	while (1)
 	{
@@ -11,6 +12,8 @@ int main(void)
 		// PRO TIP: sleep() doesn't link it if you print to stdout without using a newling '\n'
 		//	You have to flush the stream?!
 		fflush(stdout);
+		// PURELY FOR VALGRIND'S SAKE
+		// if (++i > 10) { break; }
 	}
 
 	return 0;
