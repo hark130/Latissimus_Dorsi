e300236457fd0a02b651d7df4075889191276544
hark130 <hark130@yahoo.com>
2018-03-15 23:51:51 -0500

    3-4-1: nosig.exe appears to be working PERFECTLY
    nosig.c has gone through a lot of changes since the last commit.  Most important of which is the realization (see: Resarch-and-Copy) that nohup doesn't fork so, apparently, it doesn't "lose things".  Even better, it doesn't even use a signal handler.  It merely ignores the SIG_HUP signal.  Funny, I want nosig to ignore EVERYTHING.  That fixed signals 1-31 but I was getting sigaction() failures on anything higher than 31 (say, 32).  That led to rt_sigaction.
    
    GOOD NEWS:  Now that I'm using rt_sigaction (properly), nosig can now ignore (SIG_IGN) all 62 of the SIGRTMAX (defined as 64 on my system) signals that can be ignored.
    BAD NEWS:  nosig.c is in a sad state right now.  There are more comments than statements (currently being extracted).  There are also swaths of commented code blocks.  Basically, it needs to be cleaned up.  That's next.  For now, I'm going to bask in the glory of nosig.exe WORKING.

--- 3-Internals/3-04_Signal_Handling-1_nosig.c
+++ 3-Internals/3-04_Signal_Handling-1_nosig.c
@@ -1,62 +1,13 @@
 #include <errno.h>			// errno
 #include "Harklerror.h"
-#include "Memoroad.h"		// release_a_string
 #include <signal.h>			// signals
-#include "Signaleroad.h"	// str_signaleroad
 #include <stdbool.h>		// bool, true, false
 #include <stdio.h>			// fprintf
-#include <stdlib.h>			// EXIT_SUCCESS
 #include <string.h>			// strerror
 #include <sys/stat.h>		// stat
+#include <sys/syscall.h>	// syscall
 #include <sys/types.h>
-#include <sys/wait.h>		// wait
-#include <unistd.h>			// exec*(), fork
-
-// GLOBAL VARIALBES
-int signalCaught = 0;
-
-
-static void tester(int sig)
-{
-	// // LOCAL VARIABLES
-	// char* temp_ptr = NULL;
-
-	// // RESOLVE SIGNAL
-	// temp_ptr = str_signaleroad(sig);
-
-	// if (temp_ptr)
-	// {
-	// 	fprintf(stdout, "SIGNAL %d CAUGHT!\n%s\n", sig, temp_ptr);
-	// }
-	// else
-	// {
-	// 	HARKLE_ERROR(nosig, tester, str_signaleroad failed);
-	// }
-
-	// // CLEAN UP
-	// if (temp_ptr)
-	// {
-	// 	if (false == release_a_string(&temp_ptr))
-	// 	{
-	// 		HARKLE_ERROR(nosig, tester, release_a_string failed);
-	// 	}
-	// }
-
-	// Save the signal caught
-	signalCaught = sig;
-
-	// DONE
-	return;
-}
-
-
-static void silencer(int sig)
-{
-	// fprintf(stdout, "signal %d silenced", sig);
-	// Save the signal caught
-	signalCaught = sig;
-	return;
-}
+#include <unistd.h>			// exec*()
 
 
 int main(int argc, char* argv[])
@@ -67,12 +18,8 @@ int main(int argc, char* argv[])
 	struct stat nosigFile;  // Fill this struct with stat()
 	int errNum = 0;  // Store errno here upon error
 	char* nosigFname = NULL;  // argv[argc - 1]
-	pid_t forkPID = 0;  // Store the return value from fork() here
-	pid_t tempPID = 0;  // Return value for wait()
-	int statLoc = 0;  // [OUT] parameter for wait()
 	struct sigaction sigact;  // Used to specify actions for specific signals
 	int sigNum = 1;  // Signal numbers to iterate through
-	char* signal_ptr = NULL;  // Allocate a description of the signal caught
 
 	// 1. INPUT VALIDATION
 	fprintf(stdout, "\n");
@@ -99,7 +46,7 @@ int main(int argc, char* argv[])
 	}
 	else
 	{
-		nosigFname = argv[argc - 1];
+		nosigFname = argv[1];
 	}
 
 	// 1.2. File type
@@ -118,148 +65,126 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	// 2. CREATE ARGUMENT LIST
-	////////////////////////////// IMPLEMENT LATER //////////////////////////////
-	/* TEMP */
-	char* argList[] = { nosigFname, NULL };  // Temporary solution
-
-	// 3. SIGNAL HANDLER
+	// 2. SIGNAL HANDLER == IGNORE
 	if (success == true)
 	{
 		sigemptyset(&sigact.sa_mask);  // Zeroize the mask of signals which should be blocked
 		sigact.sa_flags = 0;  // Can't think of any good flags to add for what I'm doing
-		// sigact.sa_handler = tester;  // I want to see the signals go through first
-		sigact.sa_handler = silencer;  // This is the real handler for this program
+		sigact.sa_handler = SIG_IGN;  // Apparently, instead of handling the SIGNALS, we'll just ignore them
 
 		// Set all the actions
-		for (sigNum = 1; sigNum <= 64; sigNum++)
+		for (sigNum = 1; sigNum <= SIGRTMAX; sigNum++)
+		// for (sigNum = 1; sigNum <= 64; sigNum++)
 		{
-			if (sigNum == SIGKILL || sigNum == SIGSTOP || sigNum == SIGCHLD || sigNum == 32 || sigNum == 33)
+			if (sigNum == SIGKILL || sigNum == SIGSTOP)  // || sigNum == 32 || sigNum == 33)
 			{
-				// Skipping SIGKILL == 9
-				// Skipping SIGSTOP == 19
+				// Skipping SIGKILL == 9	// Can't ignore
+				// Skipping SIGSTOP == 19	// Can't ignore
 			}
 			else
 			{
-				if (-1 == sigaction(sigNum, &sigact, NULL))
-				{			
-					HARKLE_ERROR(nosig, main, sigaction failed);
-					fprintf(stderr, "sigaction(%d, &sigact, NULL) failed\n", sigNum);
+				/* IMPORTANT NOTE */
+				// I decided to transition from sigaction() to syscall(rt_sigaction) so that
+				//	I could ignore (SIG_IGN) signals that exceeded 31.  Here's some of what I learned:
+				// 	1. rt_sigaction, the underpinings of sigaction() can handle larger sigset_t sizes (SEE: sigact.sa_mask)
+				//	2. rt_sigaction's fourth argument "is currently required to have the value sizeof(sigset_t)" (http://man7.org/linux/man-pages/man2/sigaction.2.html)
+				//  3. On my system, sizeof(sigset_t) and sizeof(sigact.sa_mask) both resolve to 128
+				//  4. syscall(13, 1, &sigact, NULL, 128) == EINVAL
+				//  5. An strace of sigaction(1, &sigact, NULL) reveals rt_sigaction(SIGHUP, {SIG_IGN, [], SA_RESTORER, 0x7fbfdcedd4b0}, NULL, 8)
+				//		(SEE: Latissimus_Dorsi/3-Internals/Error_Output/nosig03-EINVAL-sigaction-strace.txt if it still exists)
+				//	6. An strace of syscall(13, 1, &sigact, NULL, 128) reveals rt_sigaction(SIGHUP, {SIG_IGN, [], 0}, NULL, 128)
+				//		(SEE: Latissimus_Dorsi/3-Internals/Error_Output/nosig03-EINVAL-rt_sigaction-strace.txt if it still exists)
+				//	7. Observing this behavior leads me to the following conclusions
+				//		A. sigaction() has added the SA_RESTORER flag to the rt_sigaction() system call
+				//		B. SA_RESTORER - "Not intended for application use.  This flag is used by C libraries to indicate 
+				//			that the sa_restorer field contains the address of a "signal trampoline".
+				//		C. This means that 0x7fbfdcedd4b0 is likely the memory address of the "signal trampoline" sigaction is using
+				//			(SEE: SIGRETURN(2) for more on trampolines)
+				//		D. sigaction() is using the value of 8 as the value for the fourth argument.
+				//	8. Observing this behavior has raised the following questions:
+				//		A. Where is sigaction() getting "8" from?  If I had 3 guesses, here they are (in order):
+				//			i. The size of the memory address storing the signal number?  	sizeof(void*) == 8
+				//			ii. The size of &sigact?  										sizeof(&sigact) == 8
+				//			iii. sizeof(sigset_t) / 16?										sizeof(sigset_t) == 128
+				//				This one is far fetched but it's the only calculation involving sizeof(sigset_t) that works.
+				//				After all, "This argument is currently required to have the value sizeof(sigset_t)"
+				// 	9. Now we go deeper.  Supposedly, one of the advantages to programming in Linux is that it's open source.  Let's
+				//		put that to the test.
+				//		Q1: Where does sigaction() get the value of 8 from?
+				//		A1: Source code for sigaction.c calculates "_NSIG / 8" (https://code.woboq.org/userspace/glibc/sysdeps/unix/sysv/linux/sigaction.c.html)
+				//		Q2: What is "_NSIG"?
+				//		A2: (64 + 1) /* Biggest signal number + 1 (including real-time signals). */ /* From <asm/signal.h> */
+				//		Q3: Where does <asm/signal.h> get "_NSIG" from?  (I looked on my local installation at /usr/include/asm-generic/signal.h)
+				//		A3: #define _NSIG 64
+				//		Q4: Can I utilize _NSIG 64 in my code?
+				//		A4: I saw lots of warnings NOT to:
+				//			i. Regarding SIGRTMIN and SIGRTMAX, 
+							 /* These should not be considered constants from userland. */ 
+							 // (/usr/include/asm-generic/signal.h)
+				//			ii. Regarding __SIGRTMIN and __SIGRTMAX, 
+							 /* These are the hard limits of the kernel.  
+								These values should not be used directly at user level. */ 
+							 // (/usr/include/x86_64-linux-gnu/bits/signum.h)
+				//			iii. Regarding __SIGRTMIN and __SIGRTMAX, 
+							 /* Not all systems support real-time signals.  bits/signum.h indicates
+   								that they are supported by overriding __SIGRTMAX to a value greater
+   								than __SIGRTMIN.  These constants give the kernel-level hard limits,
+   								but some real-time signals may be used internally by glibc.  Do not
+   								use these constants in application code; use SIGRTMIN and SIGRTMAX
+   								(defined in signal.h) instead.  */ 
+   							 // (https://code.woboq.org/userspace/glibc/bits/signum-generic.h.html)
+				// FINAL CONCLUSION:  The answer to 8.A. is "_NSIG / 8".  What should *I* use?  It looks like "SIGRTMAX / 8".
+				// SEE: https://github.com/hark130/Latissimus_Dorsi/blob/practice/Research_Documents/3-4-1-rt_sigaction.txt
+
+
+
+				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, sizeof(sigset_t)))  // == EINVAL (because sizeof(sigset_t) == 128 on my machine)
+				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, sizeof(sigact.sa_mask)))  // == EINVAL (because sizeof(sigset_t) == 128 on my machine)
+				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, 8))  // Now, I'm just trying to replicate the rt_sigaction() call I see in strace
+				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, sizeof(sigset_t) / 16))  // Now, I'm just trying to replicate the rt_sigaction() call I see in strace
+				if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, SIGRTMAX / 8))  // Now, I'm just trying to replicate the rt_sigaction() call I see in strace
+				// if (-1 == sigaction(sigNum, &sigact, NULL))
+				{
+					errNum = errno;
+					HARKLE_ERROR(nosig, main, syscall failed);
+					fprintf(stderr, "syscall(%d, %d, &sigact, NULL, %lu) set errno to %d:\t%s\n", SYS_rt_sigaction, sigNum, sizeof(sigset_t), errNum, strerror(errNum));
+					// HARKLE_ERROR(nosig, main, sigaction failed);
+					// fprintf(stderr, "sigaction(%d, &sigact, NULL) set errno to %d:\t%s\n", sigNum, errNum, strerror(errNum));
 					success = false;
 					break;
 				}
 			}
 		}
+		// fprintf(stdout, "Pointer size is:\t%lu\n", sizeof(&sigact));  // DEBUGGING		
+		// fprintf(stdout, "SIGNAL size is:\t%lu\n", sizeof(sigNum));  // DEBUGGING
 	}
 
-	// 4. CALL IT
+	// 2.5 WAIT HERE... VALGRIND TESTING OF THE SYST_RT_SIGACTION SYSTEM CALL
+	// int i = 0;
+	// printf("Waiting...");
+	// while (1)
+	// {
+	// 	sleep(1);
+	// 	printf(".");
+	// 	// PRO TIP: sleep() doesn't link it if you print to stdout without using a newling '\n'
+	// 	//	You have to flush the stream?!
+	// 	fflush(stdout);
+	// 	// PURELY FOR VALGRIND'S SAKE
+	// 	if (++i > 10) { printf("\n"); fflush(stdout); break; }
+	// }
+
+	// 3. DON'T CALL FORK... JUST EXEC() LIKE NOHUP
 	if (success == true)
 	{
-		switch (forkPID = fork())
+		if (-1 == execv(nosigFname, argv + 1))
 		{
-			case (-1):
-				errNum = errno;
-				HARKLE_ERROR(nosig, main, fork failed);
-				fprintf(stderr, "ERROR: fork() returned %s\n\n", strerror(errNum));
-				success = false;
-			case (0):  // Child
-				if (-1 == execv(nosigFname, argList))
-				{
-					errNum = errno;
-					retVal = errNum;
-					fprintf(stderr, "ERROR: execv() returned %s\n\n", strerror(errNum));
-				}
-				else
-				{
-					_exit(EXIT_SUCCESS);
-				}
-				break;
-			default:  // Parent
-				// tempPID = wait(&statLoc);
-				tempPID = waitpid(forkPID, &statLoc, 0);
-
-				if (tempPID == -1)
-				{
-					HARKLE_ERROR(nosig, main, waitpid failed);
-					success = false;
-				}
-				else if (forkPID != tempPID)
-				{
-					HARKLE_ERROR(nosig, main, waitpid caught the wrong PID);
-					fprintf(stderr, "Expecting PID %d but wait() caught PID %d\n", forkPID, tempPID);  // DEBUGGING
-					success = false;
-				}
-
-				fprintf(stdout, "%s returned %d.\n", nosigFname, statLoc);
-
-				// RESOLVE SIGNAL CAUGHT
-				if (signalCaught)
-				{
-					signal_ptr = str_signaleroad(signalCaught);
-
-					if (signal_ptr)
-					{
-						fprintf(stdout, "%s (%d) SIGNAL CAUGHT!\n", signal_ptr, signalCaught);
-					}
-					else
-					{
-						HARKLE_ERROR(nosig, main, str_signaleroad failed);
-					}
-				}
-
-				// RESOLVE CHILD STATUS
-				if (WIFEXITED(statLoc))
-				{
-					fprintf(stdout, "%s returned normally.\n", nosigFname);
-				}
-				else
-				{
-					fprintf(stdout, "%s did not return normally.\n", nosigFname);
-				}
-
-				if (WEXITSTATUS(statLoc))
-				{
-					fprintf(stdout, "%s returned %d from main().\n", nosigFname, WEXITSTATUS(statLoc));
-				}
-
-				if (WIFSIGNALED(statLoc))
-				{
-					fprintf(stdout, "%s terminated due to the receipt of a signal that was not caught.\n", nosigFname);
-				}
-
-				if (WTERMSIG(statLoc))
-				{
-					fprintf(stdout, "%s terminated with signal number %d - %s.\n", nosigFname, WTERMSIG(statLoc), strsignal(WTERMSIG(statLoc)));
-				}
-
-				if (WIFSTOPPED(statLoc))
-				{
-					fprintf(stdout, "%s is currently stopped.\n", nosigFname);
-				}
-
-				if (WSTOPSIG(statLoc))
-				{
-					fprintf(stdout, "%d is the number of the signal that caused the %s to stop.\n", WSTOPSIG(statLoc), nosigFname);
-				}
-
-				if (WIFCONTINUED(statLoc))
-				{
-					fprintf(stdout, "%s continued from a job control stop.\n", nosigFname);
-				}
-
-				fprintf(stdout, "\n");
+			errNum = errno;
+			retVal = errNum;
+			fprintf(stderr, "ERROR: execv() returned %s\n\n", strerror(errNum));
+			success = false;
 		}
 	}
 
-	// 5. CLEAN UP
-	if (signal_ptr)
-	{
-		if (false == release_a_string(&signal_ptr))
-		{
-			HARKLE_ERROR(nosig, main, release_a_string failed);
-		}
-	}
-
-	// 5. DONE
+	// 4. DONE
 	return retVal;
 }
