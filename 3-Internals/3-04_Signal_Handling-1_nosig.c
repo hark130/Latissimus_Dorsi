d31cd49757c532b6816a32471c0c4e9589c0afa5
hark130 <hark130@yahoo.com>
2018-03-19 18:09:48 -0500

    3-4-1: Cleaned up nosig.c
    Removed numerous comments and DEBUG statements.  Nosig.exe continues to work.  Next... valgrind.

--- 3-Internals/3-04_Signal_Handling-1_nosig.c
+++ 3-Internals/3-04_Signal_Handling-1_nosig.c
@@ -72,9 +72,8 @@ int main(int argc, char* argv[])
 		sigact.sa_flags = 0;  // Can't think of any good flags to add for what I'm doing
 		sigact.sa_handler = SIG_IGN;  // Apparently, instead of handling the SIGNALS, we'll just ignore them
 
-		// Set all the actions
+		// Set ALL the actions
 		for (sigNum = 1; sigNum <= SIGRTMAX; sigNum++)
-		// for (sigNum = 1; sigNum <= 64; sigNum++)
 		{
 			if (sigNum == SIGKILL || sigNum == SIGSTOP)  // || sigNum == 32 || sigNum == 33)
 			{
@@ -83,97 +82,28 @@ int main(int argc, char* argv[])
 			}
 			else
 			{
-				/* IMPORTANT NOTE */
-				// I decided to transition from sigaction() to syscall(rt_sigaction) so that
-				//	I could ignore (SIG_IGN) signals that exceeded 31.  Here's some of what I learned:
-				// 	1. rt_sigaction, the underpinings of sigaction() can handle larger sigset_t sizes (SEE: sigact.sa_mask)
-				//	2. rt_sigaction's fourth argument "is currently required to have the value sizeof(sigset_t)" (http://man7.org/linux/man-pages/man2/sigaction.2.html)
-				//  3. On my system, sizeof(sigset_t) and sizeof(sigact.sa_mask) both resolve to 128
-				//  4. syscall(13, 1, &sigact, NULL, 128) == EINVAL
-				//  5. An strace of sigaction(1, &sigact, NULL) reveals rt_sigaction(SIGHUP, {SIG_IGN, [], SA_RESTORER, 0x7fbfdcedd4b0}, NULL, 8)
-				//		(SEE: Latissimus_Dorsi/3-Internals/Error_Output/nosig03-EINVAL-sigaction-strace.txt if it still exists)
-				//	6. An strace of syscall(13, 1, &sigact, NULL, 128) reveals rt_sigaction(SIGHUP, {SIG_IGN, [], 0}, NULL, 128)
-				//		(SEE: Latissimus_Dorsi/3-Internals/Error_Output/nosig03-EINVAL-rt_sigaction-strace.txt if it still exists)
-				//	7. Observing this behavior leads me to the following conclusions
-				//		A. sigaction() has added the SA_RESTORER flag to the rt_sigaction() system call
-				//		B. SA_RESTORER - "Not intended for application use.  This flag is used by C libraries to indicate 
-				//			that the sa_restorer field contains the address of a "signal trampoline".
-				//		C. This means that 0x7fbfdcedd4b0 is likely the memory address of the "signal trampoline" sigaction is using
-				//			(SEE: SIGRETURN(2) for more on trampolines)
-				//		D. sigaction() is using the value of 8 as the value for the fourth argument.
-				//	8. Observing this behavior has raised the following questions:
-				//		A. Where is sigaction() getting "8" from?  If I had 3 guesses, here they are (in order):
-				//			i. The size of the memory address storing the signal number?  	sizeof(void*) == 8
-				//			ii. The size of &sigact?  										sizeof(&sigact) == 8
-				//			iii. sizeof(sigset_t) / 16?										sizeof(sigset_t) == 128
-				//				This one is far fetched but it's the only calculation involving sizeof(sigset_t) that works.
-				//				After all, "This argument is currently required to have the value sizeof(sigset_t)"
-				// 	9. Now we go deeper.  Supposedly, one of the advantages to programming in Linux is that it's open source.  Let's
-				//		put that to the test.
-				//		Q1: Where does sigaction() get the value of 8 from?
-				//		A1: Source code for sigaction.c calculates "_NSIG / 8" (https://code.woboq.org/userspace/glibc/sysdeps/unix/sysv/linux/sigaction.c.html)
-				//		Q2: What is "_NSIG"?
-				//		A2: (64 + 1) /* Biggest signal number + 1 (including real-time signals). */ /* From <asm/signal.h> */
-				//		Q3: Where does <asm/signal.h> get "_NSIG" from?  (I looked on my local installation at /usr/include/asm-generic/signal.h)
-				//		A3: #define _NSIG 64
-				//		Q4: Can I utilize _NSIG 64 in my code?
-				//		A4: I saw lots of warnings NOT to:
-				//			i. Regarding SIGRTMIN and SIGRTMAX, 
-							 /* These should not be considered constants from userland. */ 
-							 // (/usr/include/asm-generic/signal.h)
-				//			ii. Regarding __SIGRTMIN and __SIGRTMAX, 
-							 /* These are the hard limits of the kernel.  
-								These values should not be used directly at user level. */ 
-							 // (/usr/include/x86_64-linux-gnu/bits/signum.h)
-				//			iii. Regarding __SIGRTMIN and __SIGRTMAX, 
-							 /* Not all systems support real-time signals.  bits/signum.h indicates
-   								that they are supported by overriding __SIGRTMAX to a value greater
-   								than __SIGRTMIN.  These constants give the kernel-level hard limits,
-   								but some real-time signals may be used internally by glibc.  Do not
-   								use these constants in application code; use SIGRTMIN and SIGRTMAX
-   								(defined in signal.h) instead.  */ 
-   							 // (https://code.woboq.org/userspace/glibc/bits/signum-generic.h.html)
-				// FINAL CONCLUSION:  The answer to 8.A. is "_NSIG / 8".  What should *I* use?  It looks like "SIGRTMAX / 8".
-				// SEE: https://github.com/hark130/Latissimus_Dorsi/blob/practice/Research_Documents/3-4-1-rt_sigaction.txt
-
-
-
-				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, sizeof(sigset_t)))  // == EINVAL (because sizeof(sigset_t) == 128 on my machine)
-				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, sizeof(sigact.sa_mask)))  // == EINVAL (because sizeof(sigset_t) == 128 on my machine)
-				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, 8))  // Now, I'm just trying to replicate the rt_sigaction() call I see in strace
-				// if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, sizeof(sigset_t) / 16))  // Now, I'm just trying to replicate the rt_sigaction() call I see in strace
-				if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, SIGRTMAX / 8))  // Now, I'm just trying to replicate the rt_sigaction() call I see in strace
-				// if (-1 == sigaction(sigNum, &sigact, NULL))
+				// rt_sigaction() doesn't work as (barely) documented
+				// Reading sigaction() source code leads me to believe that rt_sigaction wants
+				// 	SIGRTMAX / 8 as the fourth argument (NOT sizeof(sigset_t) like the man page says).
+				// It works.  For more details, read:
+				//	https://github.com/hark130/Latissimus_Dorsi/blob/practice/Research_Documents/3-4-1-rt_sigaction.txt
+				if (-1 == syscall(SYS_rt_sigaction, sigNum, &sigact, NULL, SIGRTMAX / 8))
 				{
 					errNum = errno;
 					HARKLE_ERROR(nosig, main, syscall failed);
-					fprintf(stderr, "syscall(%d, %d, &sigact, NULL, %lu) set errno to %d:\t%s\n", SYS_rt_sigaction, sigNum, sizeof(sigset_t), errNum, strerror(errNum));
-					// HARKLE_ERROR(nosig, main, sigaction failed);
-					// fprintf(stderr, "sigaction(%d, &sigact, NULL) set errno to %d:\t%s\n", sigNum, errNum, strerror(errNum));
+					fprintf(stderr, "syscall(%d, %d, &sigact, NULL, %lu) set errno to %d:\t%s\n", SYS_rt_sigaction, \
+																								  sigNum, \
+																								  (size_t)(SIGRTMAX / 8), \
+																								  errNum, \
+																								  strerror(errNum));
 					success = false;
 					break;
 				}
 			}
 		}
-		// fprintf(stdout, "Pointer size is:\t%lu\n", sizeof(&sigact));  // DEBUGGING		
-		// fprintf(stdout, "SIGNAL size is:\t%lu\n", sizeof(sigNum));  // DEBUGGING
 	}
 
-	// 2.5 WAIT HERE... VALGRIND TESTING OF THE SYST_RT_SIGACTION SYSTEM CALL
-	// int i = 0;
-	// printf("Waiting...");
-	// while (1)
-	// {
-	// 	sleep(1);
-	// 	printf(".");
-	// 	// PRO TIP: sleep() doesn't link it if you print to stdout without using a newling '\n'
-	// 	//	You have to flush the stream?!
-	// 	fflush(stdout);
-	// 	// PURELY FOR VALGRIND'S SAKE
-	// 	if (++i > 10) { printf("\n"); fflush(stdout); break; }
-	// }
-
-	// 3. DON'T CALL FORK... JUST EXEC() LIKE NOHUP
+	// 3. EXEC()
 	if (success == true)
 	{
 		if (-1 == execv(nosigFname, argv + 1))
