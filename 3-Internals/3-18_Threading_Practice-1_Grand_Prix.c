d5e0dd3e61933c306497e298ea57cfc090ef7fff
Joseph Harkleroad <hark130@yahoo.com>
2018-04-04 13:49:47 -0500

    3-18-1: Added attribute masks to main.c
    Added attribute masks to 3-18_Threading_Practice-1_Grand_Prix.c so I don't 'lose' them.

--- 3-Internals/3-18_Threading_Practice-1_Grand_Prix.c
+++ 3-Internals/3-18_Threading_Practice-1_Grand_Prix.c
@@ -140,3 +140,20 @@ int main(int argc, char** argv)
 	// DONE
 	return 0;
 }
+
+/*
+attron/attroff bit-mask attributes
+
+A_NORMAL 		Normal display (no highlight)
+A_STANDOUT 		Best highlighting mode of the terminal.
+A_UNDERLINE 	Underlining
+A_REVERSE 		Reverse video
+A_BLINK 		Blinking
+A_DIM 			Half bright
+A_BOLD 			Extra bright or bold
+A_PROTECT 		Protected mode
+A_INVIS 		Invisible or blank mode
+A_ALTCHARSET 	Alternate character set
+A_CHARTEXT 		Bit−mask to extract a character
+COLOR_PAIR(n) 	Color−pair number n
+ */
