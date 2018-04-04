55b00d4e07cf9ee74ada01f183940b13022533cb
Joseph Harkleroad <hark130@yahoo.com>
2018-04-04 11:01:25 -0500

    3-18-1: Source file initial commit
    Empty source file with a comment header containing questions and (eventually) answers.

---
+++ 3-Internals/3-18_Threading_Practice-1_Grand_Prix.c
@@ -0,0 +1,16 @@
+/*
+  The intent of this project is to 'race' threads while displaying their near-real-time status on a ncurses-drawn race track.
+  
+1. Visual Design
+	1. Racetrack
+		1. Size
+		2. Shape
+		3. "Car" indicators
+	2. Rank Bar
+		1. Size
+		2. Location
+		3. Data displayed
+	3. Final Results
+		1. Data displayed
+2. Program Design
+ */
