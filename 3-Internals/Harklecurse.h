6b1f610c15313ba97fc2179537534e7d94a508f3
Joseph Harkleroad <hark130@yahoo.com>
2018-04-04 15:35:36 -0500

    3-18-1: Updated Harklecurse.h prototype
    Updated the kill_a_window() prototype in Harklecurse.h to return an int.

--- 3-Internals/Harklecurse.h
+++ 3-Internals/Harklecurse.h
@@ -6,11 +6,13 @@
 	PURPOSE - Erase an ncurses window
 	INPUT
 		oldWin_ptr - Pointer to a WINDOW*
-	OUTPUT - None
+	OUTPUT
+		On success, ncurses.h macro OK
+		On failure, error value from ncurses (!OK)
 	NOTES:
 		This function will zeroize the original pointer variable... for safety 
  */
-void kill_a_window(WINDOW** oldWin_ptr);
+int kill_a_window(WINDOW** oldWin_ptr);
 
 
 #endif  // __HARKLECURSE__
