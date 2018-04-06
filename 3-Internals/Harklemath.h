810bfde7098050596de8eae39bc2dc1c7ec2daf1
Joseph Harkleroad <hark130@yahoo.com>
2018-04-06 11:18:39 -0500

    3-18-1: Intial Commit Harklemath.h float funcs
    Initial commit of Harklemath.h's floating point function prototypes.  Next, to define them.  I'll have to start with calc_precision() because all of the other floating point functions will call that one.  Then, we start at the top and work our way down.

--- 3-Internals/Harklemath.h
+++ 3-Internals/Harklemath.h
@@ -1,11 +1,137 @@
 #ifndef __HARKLEMATH__
 #define __HARKLEMATH__
 
+#include <stdbool.h>		// bool, true, false
+
 typedef struct cartesianCoordinate
 {
 	double xCoord;			// X coordinate
 	double yCoord;			// Y coordinate
 } cartPnt, *cartPnt_ptr;
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////// FLOATING POINT FUNCTIONS START ///////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+/*
+	PURPOSE - Determine if one double is greater than another to a certain
+		level of precision
+	INPUT
+		x - Is x greater than...
+		y - ...y...
+		precision - ...considering this many decimal places?
+	OUTPUT
+		If x > y, true
+		Otherwise, false
+ */
+bool dble_greater_than(double x, double y, int precision);
+
+
+/*
+	PURPOSE - Determine if one double is less than another to a certain
+		level of precision
+	INPUT
+		x - Is x less than...
+		y - ...y...
+		precision - ...considering this many decimal places?
+	OUTPUT
+		If x < y, true
+		Otherwise, false
+ */
+bool dble_less_than(double x, double y, int precision);
+
+
+/*
+	PURPOSE - Determine if one double is equal to another to a certain
+		level of precision
+	INPUT
+		x - Is x equal...
+		y - ...y...
+		precision - ...considering this many decimal places?
+	OUTPUT
+		If x == y, true
+		Otherwise, false
+ */
+bool dble_equal_to(double x, double y, int precision);
+
+
+/*
+	PURPOSE - Determine if one double is NOT equal to another to a certain
+		level of precision
+	INPUT
+		x - Is x NOT equal...
+		y - ...y...
+		precision - ...considering this many decimal places?
+	OUTPUT
+		If x != y, true
+		Otherwise, false
+	NOTES
+		This function calls and returns the opposite of:
+			dble_equal_to()
+ */
+bool dble_not_equal(double x, double y, int precision);
+
+
+/*
+	PURPOSE - Determine if one double is greater than or equal to
+		another to a certain level of precision
+	INPUT
+		x - Is x greater than or equal to...
+		y - ...y...
+		precision - ...considering this many decimal places?
+	OUTPUT
+		If x >= y, true
+		Otherwise, false
+	NOTES
+		This function will call the following functions in this order:
+			dble_equal_to()
+			dble_greater_than()
+ */
+bool dble_greater_than_equal_to(double x, double y, int precision);
+
+
+/*
+	PURPOSE - Determine if one double is less than another to a certain
+		level of precision
+	INPUT
+		x - Is x less than or equal to...
+		y - ...y...
+		precision - ...considering this many decimal places?
+	OUTPUT
+		If x <= y, true
+		Otherwise, false
+	NOTES
+		This function will call the following functions in this order:
+			dble_equal_to()
+			dble_less_than()
+ */
+bool dble_less_than_equal_to(double x, double y, int precision);
+
+
+/*
+	PURPOSE - Extricate precision 'mask' calculation from the floating point
+		functions
+	INPUT
+		precision - Number of decimal points of precision desired
+	OUTPUT
+		On success, a double value to be used as a comparative value
+		On failure, 0
+	NOTES
+		This function will attempt to determine/calculate the maximum level
+			of precision for the system it is compiled on.
+		I haven't yet determined how I want to handle TOO much desired
+			precision.
+		Also, I haven't yet determined how I want to handle the system's
+			maximum level of precision if the caller's desired level of
+			precision exceeds the system's maximum precision.
+ */
+double calc_precision(int precision);
+
+
+//////////////////////////////////////////////////////////////////////////////
+/////////////////////// FLOATING POINT FUNCTIONS STOP ////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////
 ////////////////////////// GEOMETRIC FUNCTIONS START /////////////////////////
