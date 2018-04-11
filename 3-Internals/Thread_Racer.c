9fe4ab9cd5fd12d230ab3a40c3c2a053972013ef
Joseph Harkleroad <hark130@yahoo.com>
2018-04-11 17:56:34 -0500

    3-18-1: Thread_Racer.c Incremental Commit
    The following functions have been defined:
        allocate_tgpRacer_ptr
        populate_tgpRacer_ptr
        allocate_tgpRacer_arr
        free_tgpRacer_ptr
    
    The following functions remain to be defined:
        free_tgpRacer_ptr
        free_tgpRacer_arr
    
    
    
    NOTE: Nothing has been tested.  All of this has been written offline.

--- 3-Internals/Thread_Racer.c
+++ 3-Internals/Thread_Racer.c
@@ -5,7 +5,13 @@
 	what was becoming lengthy "local" code outside of my main.c.
  */
 
+#include "Harklerror.h"				// HARKLE_ERROR
 #include "Thread_Racer.h"			// struct threadGrandPrixRace
+
+#ifndef THREADRACER_MAX_TRIES
+// MACRO to limit repeated allocation attempts
+#define THREADRACER_MAX_TRIES 3
+#endif  // THREADRACER_MAX_TRIES
 
 //////////////////////////////////////////////////////////////////////////////
 /////////////////////////// STRUCT FUNCTIONS START ///////////////////////////
@@ -25,6 +31,19 @@ tgpRacer_ptr allocate_tgpRacer_ptr(void)
 {
 	// LOCAL VARIABLES
 	tgpRacer_ptr retVal = NULL;
+	int numTries = 0;  // Counter for the number of allocation attempts
+	
+	// ALLOCATE
+	while (numTries < THREADRACER_MAX_TRIES && !retVal)
+	{
+		retVal = (tgpRacer_ptr)calloc(1, sizeof(tgpRacer));
+		numTries++;
+	}
+	
+	if (!retVal)
+	{
+		HARKLE_ERROR(Thread_Racer, allocate_tgpRacer_ptr, calloc failed);
+	}
 	
 	// DONE
 	return retVal;
@@ -42,10 +61,34 @@ tgpRacer_ptr allocate_tgpRacer_ptr(void)
 		This function calls allocate_tgpRacer_ptr()
 		It is the caller's responsibility to free() the memory returned		
  */
-tgpRacer_ptr allocate_tgpRacer_pt(hThrDetails_ptr structDetails)
+tgpRacer_ptr populate_tgpRacer_ptr(hThrDetails_ptr structDetails)
 {
 	// LOCAL VARIABLES
 	tgpRacer_ptr retVal = NULL;
+	bool success = true;  // Set this to false if anything fails
+	
+	// INPUT VALIDATION
+	if (!structDetails)
+	{
+		HARKLE_ERROR(Thread_Racer, allocate_tgpRacer_ptr, NULL pointer);
+		success = false;
+	}
+	
+	// ALLOCATION
+	if (true == success)
+	{
+		retVal = allocate_tgpRacer_ptr();
+		
+		if (!retVal)
+		{
+			HARKLE_ERROR(Thread_Racer, allocate_tgpRacer_ptr, allocate_tgpRacer_ptr failed);
+			success = false;
+		}
+		else
+		{
+			retVal->F1Details = structDetails;
+		}
+	}
 	
 	// DONE
 	return retVal;
@@ -69,6 +112,31 @@ tgpRacer_ptr* allocate_tgpRacer_arr(int numRacers)
 {
 	// LOCAL VARIABLES
 	tgpRacer_ptr* retVal = NULL;
+	bool success = true;  // Set this to false if anything fails
+	int numTries = 0;  // Counter for the number of allocation attempts
+	
+	// INPUT VALIDATION
+	if (numRacers < 1)
+	{
+		HARKLE_ERROR(Thread_Racer, allocate_tgpRacer_arr, allocate_tgpRacer_ptr failed);
+		success = false;
+	}
+	
+	// ALLOCATION
+	if (true == success)
+	{
+		while (numTries < THREADRACER_MAX_TRIES && !retVal)
+		{
+			retVal = (tgpRacer_ptr*)calloc(numRacers + 1, sizeof(tgpRacer_ptr));
+			numTries++;	
+		}
+		
+		if (!retVal)
+		{
+			HARKLE_ERROR(Thread_Racer, allocate_tgpRacer_arr, calloc failed);
+			success = false;
+		}
+	}
 	
 	// DONE
 	return retVal;
@@ -87,6 +155,31 @@ bool free_tgpRacer_ptr(tgpRacer_ptr* oldStruct_ptr)
 {
 	// LOCAL VARIABLES
 	bool retVal = true;
+	
+	// INPUT VALIDATION
+	if (!oldStruct_ptr || NULL == *oldStruct_ptr)
+	{
+		HARKLE_ERROR(Thread_Racer, free_tgpRacer_ptr, NULL pointer);
+		retVal = false;
+	}
+	else
+	{
+		// Zeroize the struct members		
+		// Free the struct members		
+		// NULL the struct members
+		if (false == free_a_hThrDetails_ptr(&((*oldStruct_ptr)->F1Details)))
+		{
+			HARKLE_ERROR(Thread_Racer, free_tgpRacer_ptr, free_a_hThrDetails_ptr failed);
+			retVal = false;
+			(*oldStruct_ptr)->F1Details = NULL;  // Make it NULL anyway
+		}
+		
+		// Free the struct
+		free(*oldStruct_ptr);
+		
+		// NULL the struct
+		*oldStruct_ptr = NULL;		
+	}	
 	
 	// DONE
 	return retVal;
