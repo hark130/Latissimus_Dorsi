575072b00677b8df76acff4827159f7462838b3b
hark130 <hark130@yahoo.com>
2018-04-13 00:11:14 -0500

    3-18-1: Modified Harklepipe.* w/ pipe read/write()
    Added read_a_pipe() and write_a_pipe() functions to Harklepipe.*.  Unknown if write_a_pipe() is working properly.  It's possible that read_a_pipe() is the cause of a bug I'm currently hunting down.  This bug has been commented on in the "main.c" but it has to do with read_a_pipe().
    
     Changes to be committed:
    	modified:   Harklepipe.c
    	modified:   Harklepipe.h

--- 3-Internals/Harklepipe.c
+++ 3-Internals/Harklepipe.c
@@ -1,11 +1,13 @@
 #include <errno.h>			// errno
 #include "Harklepipe.h"		// HPIPE_READ, HPIPE_WRITE
 #include "Harklerror.h"		// HARKLE_ERROR
+#include "Memoroad.h"		
 #include <stdbool.h>		// bool, true, false
 #include <stdio.h>			// fprintf()
-#include <string.h>			// strerror
-#include <unistd.h>			// pipe(), pipe2()
+#include <string.h>			// strerror()
+#include <unistd.h>			// pipe(), pipe2(), read()
 
+#define HP_BUFF_SIZE 64
 
 int make_a_pipe(int emptyPipes[2], int flags)
 {
@@ -81,6 +83,165 @@ int make_a_pipe(int emptyPipes[2], int flags)
 		{
 			close(emptyPipes[HPIPE_WRITE]);
 			emptyPipes[HPIPE_WRITE] = writeFD;
+		}
+	}
+
+	// DONE
+	return retVal;
+}
+
+
+char* read_a_pipe(int readFD, char stop)
+{
+	// LOCAL VARIABLES
+	char* retVal = NULL;
+	char localBuff[HP_BUFF_SIZE + 1] = { 0 };  // Local buffer to read into
+	char* tmp_ptr = localBuff;  // Iterate over localBuff
+	int readCnt = 0;  // Number of characters read
+	bool success = true;  // Make this false if anything fails
+	ssize_t readRetVal = 0;  // Return value from read function call
+	int errNum = 0;  // Here to capture errno on errors
+	size_t numBytes = 1;  // Number of bytes to read at one time
+
+	// INPUT VALIDATION
+	if (readFD < 0)
+	{
+		HARKLE_ERROR(Harklepipe, read_a_pipe, Invalid file descriptor);
+		success = false;
+	}
+
+	// BEGIN READING
+	while (true == success && readCnt < HP_BUFF_SIZE)
+	{
+		readRetVal = read(readFD, tmp_ptr, numBytes);
+
+		if (-1 == readRetVal)
+		{
+			errNum = errno;
+			HARKLE_ERROR(Harklepipe, read_a_pipe, read failed);
+			fprintf(stderr, "read() returned errno:\t%s\n", strerror(errNum));
+			success = false;
+		}
+		else if (numBytes != readRetVal)
+		{
+			HARKLE_ERROR(Harklepipe, read_a_pipe, read mismatch);
+			success = false;
+		}
+		else
+		{
+			readCnt++;  // One character was read
+			fprintf(stdout, "Just read a %c (%d)\n", *tmp_ptr, *tmp_ptr);  // DEBUGGING
+
+			if (0 == *tmp_ptr || '\n' == *tmp_ptr || stop == *tmp_ptr)
+			{
+				// Done reading
+				break;
+			}
+			else
+			{
+				tmp_ptr++;  // Advance to the next index
+			}
+		}
+	}
+
+	// COPY DATA
+	if (true == success)
+	{
+		// fprintf(stdout, "Done reading!\n");  // DEBUGGING
+		retVal = copy_a_string(localBuff);
+
+		if (!retVal)
+		{
+			HARKLE_ERROR(Harklepipe, read_a_pipe, copy_a_string failed);
+			success = false;
+		}
+	}
+
+	// CLEAN UP
+	// localBuff
+	if (*localBuff)
+	{
+		if (localBuff != memset(localBuff, 0x0, readCnt))
+		{
+			HARKLE_ERROR(Harklepipe, read_a_pipe, memset failed);
+		}
+	}
+
+	if (false == success)
+	{
+		if (retVal)
+		{
+			if (false == release_a_string(&retVal))
+			{
+				HARKLE_ERROR(Harklepipe, read_a_pipe, release_a_string failed);
+			}
+		}
+	}
+
+	// DONE
+	return retVal;
+}
+
+
+/*
+	PURPOSE - Write "numBytes" worth of "writeStr" into the writeFD file
+		descriptor
+	INPUT
+		writeFD - The pipe's write file descriptor
+		writeStr - Buffer to write to writeFD
+		numBytpes - Amount of data to copy from writeStr into writeFD
+	OUTPUT
+		On success, 0
+		On failure, error code
+	NOTES
+		This function will not close the file descriptor
+ */
+int write_a_pipe(int writeFD, void* writeStr, size_t numBytes)
+{
+	// LOCAL VARIABLES
+	int retVal = 0;
+	int errNum = 0;  // Here to capture errno if anything fails
+	bool success = true;  // Set this to false if anything fails
+	ssize_t writeRetVal = 0;  // Return value from read function call
+
+	// INPUT VALIDATION
+	if (writeFD < 0)
+	{
+		HARKLE_ERROR(Harklepipe, write_a_pipe, Invalid file descriptor);
+		success = false;
+	}
+	else if (!writeStr)
+	{
+		HARKLE_ERROR(Harklepipe, write_a_pipe, NULL pointer);
+		success = false;
+	}
+	else if (numBytes <= 0)
+	{
+		HARKLE_ERROR(Harklepipe, write_a_pipe, Invalid number of bytes);
+		success = false;
+	}
+
+	// WRITE
+	if (true == success)
+	{
+		writeRetVal = write(writeFD, writeStr, numBytes);
+
+		if (-1 == writeRetVal)
+		{
+			errNum = errno;
+			HARKLE_ERROR(Harklepipe, read_a_pipe, write failed);
+			fprintf(stderr, "write() returned errno:\t%s\n", strerror(errNum));
+			success = false;
+		}
+		else if (0 == writeRetVal)
+		{
+			errNum = errno;
+			HARKLE_ERROR(Harklepipe, read_a_pipe, write failed to write any bytes);
+			if (errNum)
+			{
+				fprintf(stderr, "write() returned errno:\t%s\n", strerror(errNum));
+			}
+			success = false;
 		}
 	}
 
--- 3-Internals/Harklepipe.h
+++ 3-Internals/Harklepipe.h
@@ -1,6 +1,8 @@
 #ifndef __HARKLEPIPE__
 #define __HARKLEPIPE__
 
+#include <stdlib.h>			// size_t
+
 // MACROs to help properly access int array indices
 #define HPIPE_READ 0
 #define HPIPE_WRITE 1
@@ -23,5 +25,44 @@
  */
 int make_a_pipe(int emptyPipes[2], int flags);
 
+
+/*
+	PURPOSE - Read data from a pipe's file descriptor stopping at the 'stop' char
+	INPUT
+		readFD - The pipe's read file descriptor
+		stop - A character that, if read, will cause this function
+			to stop reading and return with everything read
+			up until that point
+	OUTPUT
+		On sucess, heap-allocated copy of what was read
+		On failure, NULL
+	NOTES
+		This function will not close the file descriptor
+		It is the caller's responsibility to free the memory returned
+		It is the caller's responsibility to sychronize read and write
+			operations with whatever is on the write end of this pipe
+		This function will stop reading when it encounters a newline or a nul
+			character, regardless of what value 'stop' is
+		Maybe later we'll support some realloc functionality to
+			continue reading.  Right now, I don't need it and I
+			just want to get the bytes flowing through these pipes.
+ */
+char* read_a_pipe(int readFD, char stop);
+
+
+/*
+	PURPOSE - Write "numBytes" worth of "writeStr" into the writeFD file
+		descriptor
+	INPUT
+		writeFD - The pipe's write file descriptor
+		writeStr - Buffer to write to writeFD
+		numBytpes - Amount of data to copy from writeStr into writeFD
+	OUTPUT
+		On success, 0
+		On failure, error code
+	NOTES
+		This function will not close the file descriptor
+ */
+int write_a_pipe(int writeFD, void* writeStr, size_t numBytes);
 
 #endif  // __HARKLEPIPE__
