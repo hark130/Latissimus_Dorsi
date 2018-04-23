c951e4f1f5f11d58be3d540ac888c3497485d9a5
hark130 <hark130@yahoo.com>
2018-04-23 00:14:21 -0500

    3-22-1: injector.c EOD commit
    3-22_Process_Injection-1_injector.c is coming along.  injector.exe can currently:
    	- Parse args
    	- Validate input
    	- Parse /proc/
    	- Attack to a PID
    	- Backup that PID's registers
    	- Parse /proc/PID/maps to find executable memory
    	- Backup that memory locally
    
    To Do:
    	- 'Procure' code to inject
    	- Overwrite the memory section
    	- Update RIP to point to the injected code
    	- Resume execution of the PID
    	- Wait for a SIGTRAP
    	- Restore the PID to its full original state
    
    Everything that currently needs to be cleaned up is being cleaned up.  main() is pretty clean as-is.  So far, I've done a good job of exricating most of the functionality to files 'outside' of main.c.  There's only a single swath of DEBUGGING code (currently commented out) that I used to look at the "memory backup" because I didn't want to use gdb.

--- 3-Internals/3-22_Process_Injection-1_injector.c
+++ 3-Internals/3-22_Process_Injection-1_injector.c
@@ -42,23 +42,40 @@
 		3. Call our function
 		4. Our function operates
 		5. Then our function calls their function
+
+	NECESSARY PREPARATION:
+		"ptrace() returned errno:	Operation not permitted"
+			This is due to kernel hardening in Linux disable this behavior by:
+				`echo 0 > /proc/sys/kernel/yama/ptrace_scope`
+				modifying /etc/sysctl.d/10-ptrace.conf
+
  */
 
+#include <errno.h>							// errno
 #include "Harklerror.h"						// HARKLE_ERROR
 #include "Harkleproc.h"						// is_it_a_PID(), make_PID_into_proc()
 #include "Memoroad.h"						// release_a_string()
+#include "pmparser.h"						// pmStruct_ptr
 #include <stdbool.h>						// bool, true, false
 #include <stdio.h>							// fprintf()
 #include <string.h>							// strcmp()
-
+#include <sys/ptrace.h>						// ptrace()
+#include <sys/user.h>						// struct user_regs_struct
 
 int main(int argc, char* argv[])
 {
 	// LOCAL VARIABLES
 	bool success = true;  // Make this false if anything fails
+	long ptRetVal = 0;  // ptrace() return value
+	int errNum = 0;  // Store errno here
 	pidDetails_ptr* procPIDStructs = parse_proc_PID_structs();
 	pidDetails_ptr* temp_arr = procPIDStructs;
+	pidDetails_ptr vicPID = NULL;  // Store the struct pointer for argv[1] here
 	char* userProcPID = NULL;  // Will hold char* holding /proc/<PID>/ built from user's choice
+	struct user_regs_struct oldRegs;  // Store the state of the registers here
+	pmStruct_ptr procMaps_ptr = NULL;  // /proc/PID/maps for argv[1]
+	pmStruct_ptr tmpPM_ptr = NULL;  // Temporary variable to print out certain mappings
+	struct iovec* localBackup = NULL;  // Local backup of the PIDs executable memory map
 
 	// INPUT VALIDATION
 	// procPIDStructs
@@ -101,6 +118,7 @@ int main(int argc, char* argv[])
 				if (0 == strcmp((*temp_arr)->pidName, userProcPID) && (*temp_arr)->stillExists == true)
 				{
 					success = true;
+					vicPID = *temp_arr;
 					fprintf(stdout, "\nParsing %s (%s)\n", (*temp_arr)->pidName, (*temp_arr)->pidCmdline);
 				}
 				else
@@ -120,25 +138,89 @@ int main(int argc, char* argv[])
 	// 1. Attach to the 'victim' process
 	if (true == success)
 	{
-		//ptrace(PTRACE_ATTACH, pid, NULL, NULL)
+		ptRetVal = ptrace(PTRACE_ATTACH, vicPID->pidNum, NULL, NULL);
+
+		if (-1 == ptRetVal)
+		{
+			errNum = errno;
+			HARKLE_ERROR(injector, main, PTRACE_ATTACH failed);
+			fprintf(stderr, "ptrace() returned errno:\t%s\n", strerror(errNum));
+			success = false;
+		}
 	}
 
 	// 2. Backup the current state of the processor registers
 	if (true == success)
 	{
+		ptRetVal = ptrace(PTRACE_GETREGS, vicPID->pidNum, NULL, &oldRegs);
 
+		if (-1 == ptRetVal)
+		{
+			errNum = errno;
+			HARKLE_ERROR(injector, main, PTRACE_GETREGS failed);
+			fprintf(stderr, "ptrace() returned errno:\t%s\n", strerror(errNum));
+			success = false;
+		}
 	}
 
 	// 3. Parse /proc/PID/maps to get some "r-xp" memory
 	if (true == success)
 	{
+		// 3.1. /proc/PID/maps 
+		procMaps_ptr = pmparser_parse(vicPID->pidNum);
 
+		if (!procMaps_ptr)
+		{
+			HARKLE_ERROR(injector, main, pmparser_parse failed);
+			success = false;
+		}
+		else
+		{
+			tmpPM_ptr = procMaps_ptr;
+
+			while (tmpPM_ptr)
+			{
+				if (1 == tmpPM_ptr->is_x)
+				{
+					// pmparser_print(tmpPM_ptr, 0);  // DEBUGGING
+					// fprintf(stdout, "\n");  // DEBUGGING
+					break;  // Found one
+				}
+
+				tmpPM_ptr = tmpPM_ptr->next;
+			}
+			
+		}
 	}
 
 	// 4. Backup the memory section
 	if (true == success)
 	{
+		localBackup = copy_remote_to_local(vicPID->pidNum, tmpPM_ptr->addr_start, (size_t)tmpPM_ptr->length);
 
+		if (!localBackup)
+		{
+			HARKLE_ERROR(injector, main, copy_remote_to_local failed);
+			success = false;
+		}
+		// else
+		// {
+		// 	for (size_t i = 0; i < localBackup->iov_len; i++)
+		// 	{
+		// 		if (0 == i % 100)
+		// 		{
+		// 			fprintf(stdout, "\n");
+		// 		}
+
+		// 		// fprintf(stdout, "0x%08X ", (char)(*((char*)localBackup->iov_base + i)));
+		// 		char letter = (char)(*((char*)localBackup->iov_base + i));
+		// 		if (letter >= ' ' && letter <= '~')
+		// 		{
+		// 			fprintf(stdout, "%c", letter);
+		// 		}
+		// 	}
+		// 	fprintf(stdout, "\n");
+		// }
 	}
 
 	// 5. Overwrite executable memory section
@@ -177,7 +259,7 @@ int main(int argc, char* argv[])
 	{
 		if (false == free_PID_struct_arr(&procPIDStructs))
 		{
-			HARKLE_ERROR(injector, main, free_PID_struct_arr has failed);
+			HARKLE_ERROR(injector, main, free_PID_struct_arr failed);
 		}
 	}
 	temp_arr = NULL;
@@ -187,8 +269,23 @@ int main(int argc, char* argv[])
 	{
 		if (false == release_a_string(&userProcPID))
 		{
-			HARKLE_ERROR(print_PID_libraries, main, release_a_string has failed);
-			// fprintf(stderr, "\n<<<ERROR>>> - print_PID_libraries - release_a_string has failed!\n\n");
+			HARKLE_ERROR(injector, main, release_a_string failed);
+		}
+	}
+
+	// 3. 
+	if (procMaps_ptr)
+	{
+		pmparser_free(procMaps_ptr);
+		procMaps_ptr = NULL;
+	}
+
+	// 4. localBackup
+	if (localBackup)
+	{
+		if (false == free_iovec_struct(&localBackup, true))
+		{
+			HARKLE_ERROR(injector, main, free_iovec_struct failed);
 		}
 	}
 
