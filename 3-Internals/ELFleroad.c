4e1f677d7e88a32307dba78f274c482deb8e09f7
Joseph Harkleroad <hark130@yahoo.com>
2018-11-18 17:24:44 -0600

    3-10-3: Initial Commit ELFleroad.*
    Initial commit of a new header and definition file for more basic manipulation of ELF-formatted files.  Will replicate functionality found in Elf_Manipulation.* without utilizing Map_Memory.h.  Basic parsing.
    
    Prototyped and stubbed out first duplicate functionality: is_elf()
    Undefined.
    
    Changes to be committed:
    	new file:   ELFleroad.c
    	new file:   ELFleroad.h

---
+++ 3-Internals/ELFleroad.c
@@ -0,0 +1,16 @@
+#include "ELFleroad.h"
+
+bool is_elf(char *filename)
+{
+	// LOCAL VARIABLES
+	bool retVal = false;
+
+	// INPUT VALIDATION
+	if (filename && *filename)
+	{
+		// CHECK FILE	
+	}	
+
+	// DONE
+	return retVal;
+}
---
+++ 3-Internals/ELFleroad.h
@@ -0,0 +1,25 @@
+/*
+	This header will be a collection of basic functions to parse ELF-formatted files.  It is
+		similar in function to Elf_Manipulation.h but will not be utilizing the Map_Memory.h
+		helper functions/structs.  Here, we're doing it live!
+ */
+
+#ifndef __ELFEROAD__
+#define __ELFEROAD__
+
+#include <elf.h>
+#include <stdbool.h>							// bool, true, false
+
+
+/*
+	PURPOSE - Check a file for the ELF Magic Number
+	INPUT
+		filename - C String to a relative or absolute file
+	OUTPUT
+		true if is in ELF format
+		Otherwise, false
+ */
+bool is_elf(char *filename);
+
+
+#endif  // __ELFEROAD__
