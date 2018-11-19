62c1380ef0909683e34b5845885aefe393c668c8
Joseph Harkleroad <hark130@yahoo.com>
2018-11-19 16:33:27 -0600

    3-10-3: Defined ELFleroad map_file()
    Defined map_file() in ELFleroad.c.  Unlike Map_Memory's map_file():
        - O_RDONLY
        - No verification it's a regular file
        - No mapMem struct... just a void*
    
    Written offline.  Untested.  (especially the includes)

--- 3-Internals/ELFleroad.c
+++ 3-Internals/ELFleroad.c
@@ -1,7 +1,64 @@
 #include "ELFleroad.h"
+#include <errno.h>								// errno
+#include <fcntl.h>								// O_RDONLY
 #include "Fileroad.h"
 #include "Harklerror.h"
 #include <string.h>								// strstr()
+#include <sys/mman.h>							// mmap(), PROT_READ
+#include <sys/stat.h>							// open()
+
+
+void *map_file(char *filename)
+{
+	// LOCAL VARIABLES
+	void *retVal = NULL;
+	int fileDesc = 0;  // File descriptor for filename
+	struct stat fileStat;  // Store the file statistics here
+	int errNum = 0;  // Store errno here
+	
+	// INPUT VALIDATION
+	if (filename && *filename)
+	{
+		// MAP IT
+		// 1. Get file descriptor
+		fileDes = open(filename, O_RDONLY);
+		
+		if (0 > fileDesc)
+		{
+			HARKLE_ERROR(ELFleroad, map_file, Unable to open filename);	
+		}
+		else
+		{
+			// 2. Get the file status
+			if (fstat(fileDesc, &fileStat))
+			{
+				HARKLE_ERROR(ELFleroad, map_file, Unable to fstat filename);	
+			}
+			else
+			{
+				if (0 >= fileStat.st_size)
+				{
+					HARKLE_ERROR(ELFleroad, map_file, Invalid file size);	
+				}
+				else
+				{
+					retVal = mmap(NULL, fileStat.st_size, PROT_READ, MAP_SHARED, fileDesc, 0);
+					
+					if (!retVal || MAP_FAILED == retVal)
+					{
+						errNum = errno;
+						retVal = NULL;
+						HARKLE_ERROR(ELFleroad, map_file, mmap failed);
+						HARKLE_ERRNO(ELFleroad, mmap, errNum);
+					}
+				}
+			}
+		}
+	}
+	
+	// DONE
+	return retVal;
+}
 
 
 bool is_elf(char *filename)
