21c2c84b9013e7fc21288f4220af75c371863cb7
Joseph Harkleroad <hark130@yahoo.com>
2018-11-19 15:54:59 -0600

    3-10-3: Implemented ELFleroad's is_elf()
    Defined is_elf() in ELFleroad.c.
    Written offline.  Untested.

--- 3-Internals/ELFleroad.c
+++ 3-Internals/ELFleroad.c
@@ -1,15 +1,34 @@
 #include "ELFleroad.h"
+#include "Fileroad.h"
 #include "Harklerror.h"
 
 bool is_elf(char *filename)
 {
 	// LOCAL VARIABLES
 	bool retVal = false;
+	char *fileCont = NULL;
+	char magicNum[] = { 0x7F, 'E', 'L', 'F', 0x0 };
 
 	// INPUT VALIDATION
 	if (filename && *filename)
 	{
-		// CHECK FILE	
+		// CHECK FILE
+		fileCont = fread_a_file(filename);
+		
+		if (fileCont)
+		{
+			if (*fileCont)
+			{
+				if (!strcmp(fileCont, magicNum))
+				{
+					retVal = true;	
+				}
+			}
+		}
+		else
+		{
+			HARKLE_ERROR(ELFleroad, is_elf, fread_a_file failed to read a file);
+		}
 	}	
 	else
 	{
